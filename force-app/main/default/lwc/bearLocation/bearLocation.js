a08fa4b08fa68845265a93d2e085d0e186a3e3db
Mahesh Dandu <69712258+maheshdandu346@users.noreply.github.com>
2020-10-20 10:23:44 +0530

    4th commit

---
+++ force-app/main/default/lwc/bearLocation/bearLocation.js
@@ -0,0 +1,35 @@
+import { LightningElement } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+const NAME_FIELD = 'Bear__c.Name';
+const LOCATION_LATITUDE_FIELD = 'Bear__c.Location__Latitude__s';
+const LOCATION_LONGITUDE_FIELD = 'Bear__c.Location__Longitude__s';
+const bearFields = [
+	NAME_FIELD,
+	LOCATION_LATITUDE_FIELD,
+	LOCATION_LONGITUDE_FIELD
+];
+export default class BearLocation extends LightningElement {
+
+    name;
+    mapMarkers = [];
+   
+    loadBear({ error, data }) {
+      if (error) {
+        // TODO: handle error
+      } else if (data) {
+        // Get Bear data
+        this.name =  getFieldValue(data, NAME_FIELD);
+        const Latitude = getFieldValue(data, LOCATION_LATITUDE_FIELD);
+        const Longitude = getFieldValue(data, LOCATION_LONGITUDE_FIELD);
+        // Transform bear data into map markers
+        this.mapMarkers = [{
+          location: { Latitude, Longitude },
+          title: this.name,
+          description: `Coords: ${Latitude}, ${Longitude}`
+        }];
+      }
+    }
+    get cardTitle() {
+      return (this.name) ? `${this.name}'s location` : 'Bear location';
+    }
+}
